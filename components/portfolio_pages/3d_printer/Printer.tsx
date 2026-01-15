bf59170fd3baa49c4b652d3f410c0928d36ddaba
CreatorLogs <creatorlogs.official@gmail.com>
2026-01-14 20:57:20 -0600

    added 3d printer page

---
+++ components/portfolio_pages/3d_printer/Printer.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Printer: React.FC = () => {
+  return (<section
+      id="contact"
+      className="scroll-mt-32 max-w-2xl mx-auto text-center md:text-left"
+    ></section>);}
